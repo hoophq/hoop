d955a581869bebba159829b48a7221ecfd0ae87c
danielbonilha <dani.bonilha@gmail.com>
2022-11-01 16:12:57 -0300

    added agent registration url (#45)
    * added agent registration url
    
    * removed variable checks
    
    * removed port from server address

--- agent/main.go
+++ agent/main.go
@@ -12,9 +12,11 @@ import (
 
 func Run() {
 	fmt.Println(string(version.JSON()))
+
+	svrAddr := os.Getenv("SERVER_ADDRESS")
 	token := os.Getenv("TOKEN")
 
-	client, err := grpc.Connect(os.Getenv("SERVER_ADDRESS"), token, grpc.WithOption("origin", pb.ConnectionOriginAgent))
+	client, err := grpc.Connect(svrAddr, token, grpc.WithOption("origin", pb.ConnectionOriginAgent))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +26,7 @@ func Run() {
 	agt := New(client, done)
 	defer agt.Close()
 
-	go agt.Run()
+	go agt.Run(svrAddr, token)
 	<-ctx.Done()
 	if err := ctx.Err(); err != nil {
 		log.Printf("error: %s", err.Error())
