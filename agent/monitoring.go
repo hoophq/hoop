79f073d5c978568c64bbace4fd4cea03bac87b1e
San <sandromll@gmail.com>
2024-02-14 14:10:04 -0300

    remove profiler

--- agent/monitoring.go
+++ agent/monitoring.go
@@ -1,7 +1,6 @@
 package agent
 
 import (
-	"github.com/pyroscope-io/client/pyroscope"
 	"github.com/runopsio/hoop/common/log"
 	"github.com/runopsio/hoop/common/monitoring"
 	pb "github.com/runopsio/hoop/common/proto"
@@ -10,11 +9,6 @@ import (
 func (a *Agent) startMonitoring(pkt *pb.Packet) {
 	if len(pkt.Payload) == 0 {
 		return
-	}
-	// pyroscope setup
-	if prof, _ := a.connStore.Get(profilerInstanceKey).(*pyroscope.Profiler); prof != nil {
-		log.Infof("profiler - found a profiler instance, stopping it ...")
-		_ = prof.Stop()
 	}
 
 	var conf monitoring.TransportConfig
