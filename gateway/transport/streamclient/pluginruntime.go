4f6be5e541dce54286a00dea310747f5048a86ed
San <sandromll@gmail.com>
2024-04-13 20:47:24 -0300

    add missing files

---
+++ gateway/transport/streamclient/pluginruntime.go
@@ -0,0 +1,107 @@
+package streamclient
+
+import (
+	"github.com/runopsio/hoop/common/log"
+	pb "github.com/runopsio/hoop/common/proto"
+	pgplugins "github.com/runopsio/hoop/gateway/pgrest/plugins"
+	pluginsslack "github.com/runopsio/hoop/gateway/transport/plugins/slack"
+	plugintypes "github.com/runopsio/hoop/gateway/transport/plugins/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type runtimePlugin struct {
+	plugintypes.Plugin
+	config []string
+}
+
+func loadRuntimePlugins(ctx plugintypes.Context) ([]runtimePlugin, error) {
+	pluginsConfig := make([]runtimePlugin, 0)
+	var nonRegisteredPlugins []string
+	for _, p := range plugintypes.RegisteredPlugins {
+		p1, err := pgplugins.New().FetchOne(ctx, p.Name())
+		if err != nil {
+			log.Errorf("failed retrieving plugin %q, err=%v", p.Name(), err)
+			return nil, status.Errorf(codes.Internal, "failed registering plugins")
+		}
+		if p1 == nil {
+			nonRegisteredPlugins = append(nonRegisteredPlugins, p.Name())
+			continue
+		}
+
+		if p.Name() == plugintypes.PluginSlackName {
+			if p1.Config != nil {
+				ctx.ParamsData[pluginsslack.PluginConfigEnvVarsParam] = p1.Config.EnvVars
+			}
+		}
+
+		for _, c := range p1.Connections {
+			if c.Name == ctx.ConnectionName {
+				config := removePluginConfigDuplicates(c.Config)
+				ep := runtimePlugin{
+					Plugin: p,
+					config: config,
+				}
+
+				if err = p.OnConnect(ctx); err != nil {
+					log.Warnf("plugin %q refused to accept connection %q, err=%v", p1.Name, ctx.SID, err)
+					return pluginsConfig, status.Errorf(codes.FailedPrecondition, err.Error())
+				}
+
+				pluginsConfig = append(pluginsConfig, ep)
+				break
+			}
+		}
+	}
+	if len(nonRegisteredPlugins) > 0 {
+		log.With("sid", ctx.SID).Infof("non registered plugins %v", nonRegisteredPlugins)
+	}
+	return pluginsConfig, nil
+}
+
+func (s *ProxyStream) PluginExecOnReceive(pctx plugintypes.Context, pkt *pb.Packet) (*plugintypes.ConnectResponse, error) {
+	var response *plugintypes.ConnectResponse
+	for _, p := range s.runtimePlugins {
+		pctx.PluginConnectionConfig = p.config
+		resp, err := p.OnReceive(pctx, pkt)
+		if err != nil {
+			return nil, err
+		}
+		if resp != nil && response == nil {
+			response = resp
+		}
+	}
+	return response, nil
+}
+
+func (s *ProxyStream) PluginExecOnDisconnect(ctx plugintypes.Context, errMsg error) error {
+	for _, p := range s.runtimePlugins {
+		if err := p.OnDisconnect(ctx, errMsg); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// GetRedactInfoTypes return the in memory info types for the data masking (dlp) plugin
+func (s *ProxyStream) GetRedactInfoTypes() []string {
+	var infoTypes []string
+	for _, p := range s.runtimePlugins {
+		if p.Plugin.Name() == plugintypes.PluginDLPName {
+			infoTypes = p.config
+		}
+	}
+	return infoTypes
+}
+
+func removePluginConfigDuplicates(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := make([]string, 0)
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
