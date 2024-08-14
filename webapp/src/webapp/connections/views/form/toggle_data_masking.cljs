9c8fb3c343a06ac70bd36800d5e4f14b8752abbc
João de Sant Ana <joaohdesantana@gmail.com>
2024-08-14 09:41:50 -0300

    Add dlp-info-types-options

--- webapp/src/webapp/connections/views/form/toggle_data_masking.cljs
+++ webapp/src/webapp/connections/views/form/toggle_data_masking.cljs
@@ -3,8 +3,40 @@
             [clojure.string :as cs]
             [webapp.components.headings :as h]
             [webapp.components.multiselect :as multi-select]
-            [webapp.components.toggle :as toggle]
-            [webapp.plugins.views.plugin-configurations.dlp :as dlp-config]))
+            [webapp.components.toggle :as toggle]))
+(def dlp-info-types-options
+  ["PHONE_NUMBER",
+   "CREDIT_CARD_NUMBER",
+   "AUTH_TOKEN",
+   "AWS_CREDENTIALS",
+   "AZURE_AUTH_TOKEN",
+   "BASIC_AUTH_HEADER",
+   "ENCRYPTION_KEY",
+   "GCP_API_KEY",
+   "GCP_CREDENTIALS",
+   "JSON_WEB_TOKEN",
+   "HTTP_COOKIE",
+   "OAUTH_CLIENT_SECRET",
+   "PASSWORD",
+   "SSL_CERTIFICATE",
+   "STORAGE_SIGNED_POLICY_DOCUMENT",
+   "STORAGE_SIGNED_URL",
+   "WEAK_PASSWORD_HASH",
+   "XSRF_TOKEN",
+   "CREDIT_CARD_TRACK_NUMBER",
+   "EMAIL_ADDRESS",
+   "IBAN_CODE",
+   "HTTP_COOKIE",
+   "IMEI_HARDWARE_ID",
+   "IP_ADDRESS",
+   "STORAGE_SIGNED_URL",
+   "URL",
+   "VEHICLE_IDENTIFICATION_NUMBER",
+   "BRAZIL_CPF_NUMBER",
+   "AMERICAN_BANKERS_CUSIP_ID",
+   "FDA_CODE",
+   "US_PASSPORT",
+   "US_SOCIAL_SECURITY_NUMBER"])
 
 (defn array->select-options [array]
   (mapv #(into {} {"value" % "label" (cs/lower-case (cs/replace % #"_" " "))}) array))
@@ -36,7 +68,7 @@
                               (reset! data-masking-toggle-enabled?
                                       (not @data-masking-toggle-enabled?)))}]]
    (when @data-masking-toggle-enabled?
-     [multi-select/main {:options (array->select-options dlp-config/dlp-info-types-options)
+     [multi-select/main {:options (array->select-options dlp-info-types-options)
                          :id "data-masking-groups-input"
                          :name "data-masking-groups-input"
                          :disabled? (or (not @data-masking-toggle-enabled?) free-license?)
