b1eeff837cf46f0cfcee35b072f7b1186759acf6
Matheus Marsiglio <github@mtmr0x.com>
2024-09-14 00:40:41 -0300

    feature(auth) local auth frontend login and register basic UI

--- webapp/src/webapp/auth/local/login.cljs
+++ webapp/src/webapp/auth/local/login.cljs
@@ -1,29 +1,51 @@
 (ns webapp.auth.local.login
   (:require
-    ["@radix-ui/themes" :refer [TextField Section Card
-                                Button]]
+    ["@radix-ui/themes" :refer [Flex Card Heading
+                                Link Box Text]]
+    [webapp.components.button :as button]
+    [webapp.components.forms :as forms]
     [reagent.core :as r]
     [re-frame.core :as re-frame]))
 
-(defn panel []
+(defn- form []
   (let [email (r/atom "")
         password (r/atom "")
         loading (r/atom false)]
     (fn []
-      (println :loading @loading)
-      [:<>
-       [:> Section
-        [:> Card
-         [:> TextField.Root {:placeholder "some@email.com"
-                             :value @email
-                             :type "email"
-                             :onChange #(reset! email (-> % .-target .-value))}]
-         [:> TextField.Root {:placeholder "Password"
-                             :value @password
-                             :type "password"
-                             :onChange #(reset! password (-> % .-target .-value))}]
-         [:> Button
-          {:onClick #(re-frame/dispatch [:localauth->login {:email @email
-                                                            :password @password}])}
-          "Login"]]]])))
+      [:> Box
+       [:> Flex {:direction "column"}
+        [forms/input {:label "Email"
+                      :value @email
+                      :type "email"
+                      :on-change #(reset! email (-> % .-target .-value))}]
+        [forms/input {:label "Password"
+                      :value @password
+                      :type "password"
+                      :on-change #(reset! password (-> % .-target .-value))}]
+        [button/tailwind-primary
+         {:text "Login"
+          :disabled @loading
+          :on-click #(re-frame/dispatch [:localauth->login {:email @email
+                                                            :password @password}])}]
+        [:> Flex {:align "center" :justify "center" :class "mt-4"}
+         [:> Text {:as "div" :size "2" :color "gray-500"}
+          "Don't have an account?"
+          [:> Link {:href "/register" :class "text-blue-500 ml-1"}
+           "Create one"]]
+         ]]])))
+
+(defn panel []
+  (fn []
+    [:<>
+     [:> Flex {:align "center"
+               :justify "center"
+               :height "100vh"
+               :class "bg-gray-100"}
+      [:> Box {:width "90%" :maxWidth "380px"}
+       [:> Card {:size "4" :variant "surface" :class "bg-white"}
+        [:img {:src "/images/hoop-branding/SVG/hoop-symbol_black.svg"
+               :class "w-12 mx-auto mb-6 mt-4"}]
+        [:> Heading {:size "5" :align "center" :mb "5"}
+         "Login"]
+        [form]]]]]))
 
--- webapp/src/webapp/auth/local/register.cljs
+++ webapp/src/webapp/auth/local/register.cljs
@@ -1,29 +1,62 @@
 (ns webapp.auth.local.register
   (:require
-   ["@radix-ui/themes" :refer [TextField Section Card
-                               Button]]
-   [reagent.core :as r]
-   [re-frame.core :as re-frame]))
+    ["@radix-ui/themes" :refer [Flex Card Heading
+                                Link Box Text]]
+    [webapp.components.button :as button]
+    [webapp.components.forms :as forms]
+    [reagent.core :as r]
+    [re-frame.core :as re-frame]))
 
-(defn panel []
+(defn- form []
   (let [email (r/atom "")
+        fullname (r/atom "")
         password (r/atom "")
+        confirm-password (r/atom "")
         loading (r/atom false)]
     (fn []
-      (println :loading @loading)
-      [:<>
-       [:> Section
-        [:> Card
-         [:> TextField.Root {:placeholder "some@email.com"
-                             :value @email
-                             :type "email"
-                             :onChange #(reset! email (-> % .-target .-value))}]
-         [:> TextField.Root {:placeholder "Password"
-                             :value @password
-                             :type "password"
-                             :onChange #(reset! password (-> % .-target .-value))}]
-         [:> Button
-          {:onClick #(re-frame/dispatch [:localauth->register {:email @email
-                                                               :password @password}])}
-          "Register"]]]])))
+      [:> Box
+       [:> Flex {:direction "column"}
+        [forms/input {:label "Full name"
+                      :value @fullname
+                      :type "text"
+                      :on-change #(reset! fullname (-> % .-target .-value))}]
+        [forms/input {:label "Email"
+                      :value @email
+                      :type "email"
+                      :on-change #(reset! email (-> % .-target .-value))}]
+        [forms/input {:label "Password"
+                      :value @password
+                      :type "password"
+                      :on-change #(reset! password (-> % .-target .-value))}]
+        [forms/input {:label "Confirm Password"
+                      :value @confirm-password
+                      :type "password"
+                      :on-change #(reset! confirm-password (-> % .-target .-value))}]
+        [button/tailwind-primary
+         {:text "Register"
+          :disabled @loading
+          :on-click #(re-frame/dispatch [:localauth->register {:email @email
+                                                              :name @fullname
+                                                              :password @password}])}]
+        [:> Flex {:align "center" :justify "center" :class "mt-4"}
+         [:> Text {:as "div" :size "2" :color "gray-500"}
+          "Already have an account?"
+          [:> Link {:href "/login" :class "text-blue-500 ml-1"}
+           "Login"]]
+         ]]])))
+
+(defn panel []
+  (fn []
+    [:<>
+     [:> Flex {:align "center"
+               :justify "center"
+               :height "100vh"
+               :class "bg-gray-100"}
+      [:> Box {:width "90%" :maxWidth "380px"}
+       [:> Card {:size "4" :variant "surface" :class "bg-white"}
+        [:img {:src "/images/hoop-branding/SVG/hoop-symbol_black.svg"
+               :class "w-12 mx-auto mb-6 mt-4"}]
+        [:> Heading {:size "5" :align "center" :mb "5"}
+         "Create an account"]
+        [form]]]]]))
 
