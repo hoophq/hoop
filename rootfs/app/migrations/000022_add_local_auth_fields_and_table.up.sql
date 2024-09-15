7ca8b7cb16fffd1a74254bf6599afa317d280e65
Matheus Marsiglio <github@mtmr0x.com>
2024-09-15 11:17:47 -0300

    refactor(migration) remove reference

--- rootfs/app/migrations/000022_add_local_auth_fields_and_table.up.sql
+++ rootfs/app/migrations/000022_add_local_auth_fields_and_table.up.sql
@@ -6,7 +6,7 @@ ALTER TABLE users ADD COLUMN password TEXT;
 
 CREATE TABLE local_auth_sessions (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(id),
+    user_id UUID NOT NULL,
     user_email TEXT NOT NULL,
     token TEXT,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL
