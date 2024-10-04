31705c9ef622a6af21e4823d25123e3b6ffc7d61
Matheus Marsiglio <github@mtmr0x.com>
2024-10-04 16:07:38 -0300

    refactor(database) search path

--- gateway/models/database.go
+++ gateway/models/database.go
@@ -14,14 +14,19 @@ var DB *gorm.DB
 
 func InitDatabase() {
 	log.Info("initializing database connection")
-	dsn := appconfig.Get().PgURI() + "&search_path=private"
-	sqlDB, err := sql.Open("pgx", dsn)
 
+	dsn := appconfig.Get().PgURI()
+
+	sqlDB, err := sql.Open("pgx", dsn)
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		Conn: sqlDB,
 	}), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	err = db.Exec("SET search_path TO private").Error
+	if err != nil {
+		log.Fatalf("Failed to set schema: %v", err)
 	}
 
 	DB = db
