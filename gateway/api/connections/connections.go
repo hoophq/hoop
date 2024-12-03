18d1dabf5aecbc6db7bd9f89e731334d63915824
Rogerio Moura <roger0.rm@gmail.com>
2024-12-03 14:19:22 -0300

    feat(openapi): generate api docs

--- gateway/api/connections/connections.go
+++ gateway/api/connections/connections.go
@@ -401,7 +401,7 @@ func FetchByName(ctx pgrest.Context, connectionName string) (*models.Connection,
 //	@Tags			Core
 //	@Produce		json
 //	@Param			nameOrID	path	string	true	"Name or UUID of the connection"
-//	@Success		200			{object}	DatabaseListResponse
+//	@Success		200			{object}	openapi.ConnectionDatabaseListResponse
 //	@Failure		400,404,500	{object}	openapi.HTTPError
 //	@Router			/connections/{nameOrID}/databases [get]
 func ListDatabases(c *gin.Context) {
@@ -527,7 +527,7 @@ printjson(result);`
 //	@Produce		json
 //	@Param			nameOrID	path	string	true	"Name or UUID of the connection"
 //	@Param			database	path	string	true	"Name of the database"
-//	@Success		200			{object}	SchemaResponse
+//	@Success		200			{object}	openapi.ConnectionSchemaResponse
 //	@Failure		400,404,500	{object}	openapi.HTTPError
 //	@Router			/connections/{nameOrID}/schemas [get]
 func GetDatabaseSchemas(c *gin.Context) {
