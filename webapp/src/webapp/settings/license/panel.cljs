5b9373f6dea06412fd14530457ab971eaba90f93
Matheus Marsiglio <github@mtmr0x.com>
2024-12-04 10:13:20 -0300

    wip(license) form

--- webapp/src/webapp/settings/license/panel.cljs
+++ webapp/src/webapp/settings/license/panel.cljs
@@ -1,9 +1,10 @@
 (ns webapp.settings.license.panel
   (:require
     [re-frame.core :as rf]
-    ["@radix-ui/themes" :refer [Table Flex Box Text Heading Card]]
-    ["lucide-react" :refer [CircleCheck]]
-    [webapp.components.headings :as h]))
+    [reagent.core :as r]
+    ["@radix-ui/themes" :refer [Table Flex Box Text Heading]]
+    [webapp.components.headings :as h]
+    [webapp.components.forms :as forms]))
 
 (defmulti license-status-text identity)
 (defmethod license-status-text "oss" [_]
@@ -24,13 +25,70 @@
          (when (< day 10) "0")
          (.getDate date))))
 
+(defn information-table [license-info]
+  (let [license-type (:type license-info)
+        issued-date (:issued_at license-info)
+        expiration-date (:expire_at license-info)]
+    [:> Flex {:direction :column
+              :gap "4"}
+     [:> Box
+      [:> Heading {:size "5"}
+       "Information"]]
+     [:> Box
+      [:> Table.Root {:variant "surface"}
+       [:> Table.Header
+        [:> Table.Row
+         [:> Table.ColumnHeaderCell "Type"]
+         [:> Table.ColumnHeaderCell "Issued"]
+         [:> Table.ColumnHeaderCell "Expiration"]
+         ]]
+       [:> Table.Body
+        [:> Table.Row
+         [:> Table.Cell
+          (license-status-text license-type)]
+         [:> Table.Cell
+          [:> Text {:size "1"}
+           (get-formatted-date issued-date)]]
+         [:> Table.Cell
+          [:> Text {:size "1"}
+           (cond
+             (= license-type "oss") "N/A"
+             (= license-type "enterprise") (get-formatted-date expiration-date)
+             :else "Loading...")]]]]]]]))
+
+(defn license-table [license-info]
+  (let [license-value (r/atom "")]
+    (fn [license-info]
+      (let [is-valid? (:is_valid license-info)
+            license-type (:type license-info)]
+        [:> Flex {:direction :column
+                  :gap "4"}
+         [:> Box
+          [:> Heading {:size "5" :as "h3"}
+           "License"]]
+         [:> Box
+          [:> Table.Root {:variant "surface"}
+           [:> Table.Body
+            [:> Table.Row
+             [:> Table.ColumnHeaderCell
+              [:> Text {:size "1"} "Verified hostname"]]
+             [:> Table.Cell
+              [:> Text (:verified_host license-info)]]]
+            [:> Table.Row
+             [:> Table.ColumnHeaderCell
+              [:> Text {:size "1"} "Key ID"]]
+             [:> Table.Cell
+              (:key_id license-info)]]
+            [:> Table.Row
+             [:> Table.ColumnHeaderCell
+              [:> Text {:size "1"} "License Key"]]
+             [:> Table.Cell
+              [forms/input {:value ""}]]]]]]]))))
+
 (defn main []
   (let [gateway-info (rf/subscribe [:gateway->info])]
     (fn []
-      (let [license-info (-> @gateway-info :data :license_info)
-            license-type (:type license-info)
-            issued-date (:issued_at license-info)
-            expiration-date (:expire_at license-info)]
+      (let [license-info (-> @gateway-info :data :license_info)]
         [:div
          [:> Flex {:class "mb-10"
                    :as "header"}
@@ -40,53 +98,5 @@
             "Manage your organization's license"]]]
          [:> Flex {:direction :column
                    :gap "8"}
-          [:> Flex {:direction :column
-                    :gap "4"}
-           [:> Box
-            [:> Heading {:size "5"}
-             "Information"]]
-           [:> Box
-            [:> Table.Root {:variant "surface"}
-             [:> Table.Header
-              [:> Table.Row
-               [:> Table.ColumnHeaderCell "Type"]
-               [:> Table.ColumnHeaderCell "Issued"]
-               [:> Table.ColumnHeaderCell "Expiration"]
-               ]]
-             [:> Table.Body
-              [:> Table.Row
-               [:> Table.Cell
-                (license-status-text license-type)]
-               [:> Table.Cell
-                [:> Text {:size "1"}
-                 (get-formatted-date issued-date)]]
-               [:> Table.Cell
-                [:> Text {:size "1"}
-                 (cond
-                   (= license-type "oss") "N/A"
-                   (= license-type "enterprise") (get-formatted-date expiration-date)
-                   :else "Loading...")]]]]]]]
-          [:> Flex {:direction :column
-                    :gap "4"}
-           [:> Box
-            [:> Heading {:size "5" :as "h3"}
-             "License"]]
-           [:> Box
-            [:> Table.Root {:variant "surface"}
-             [:> Table.Body
-              [:> Table.Row
-               [:> Table.ColumnHeaderCell
-                [:> Text {:size "1"} "Verified hostname"]]
-               [:> Table.Cell
-                [:> Text (:verified_host license-info)]]]
-              [:> Table.Row
-               [:> Table.ColumnHeaderCell
-                [:> Text {:size "1"} "Key ID"]]
-               [:> Table.Cell
-                (:key_id license-info)]]
-              [:> Table.Row
-               [:> Table.ColumnHeaderCell
-                [:> Text {:size "1"} "License Key"]]
-               [:> Table.Cell
-                "*****"]]
-              ]]]]]]))))
+          [information-table license-info]
+          [license-table license-info]]]))))
