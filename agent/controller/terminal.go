748e9b11ae675aff2401fd2a3a9fa3dc222a1fe7
San <sandromll@gmail.com>
2025-01-30 09:40:54 -0300

    Improve handling exit code through agent and gateway Improve hoop connect to proper restore the term when receiving an interrupt from the OS

--- agent/controller/terminal.go
+++ agent/controller/terminal.go
@@ -15,26 +15,26 @@ import (
 )
 
 func (a *Agent) doTerminalWriteAgentStdin(pkt *pb.Packet) {
-	sessionID := string(pkt.Spec[pb.SpecGatewaySessionID])
-	connParams := a.connectionParams(sessionID)
+	sid := string(pkt.Spec[pb.SpecGatewaySessionID])
+	connParams := a.connectionParams(sid)
 	if connParams == nil {
-		log.Infof("session=%s - connection params not found", sessionID)
-		a.sendCloseTerm(sessionID, "internal error, connection params not found", "1")
+		log.With("sid", sid).Infof("connection params not found")
+		a.sendCloseTerm(sid, "internal error, connection params not found", internalExitCode)
 		return
 	}
-	sessionIDKey := fmt.Sprintf(cmdStoreKey, sessionID)
+	sessionIDKey := fmt.Sprintf(cmdStoreKey, sid)
 	cmdObj := a.connStore.Get(sessionIDKey)
 	cmd, ok := cmdObj.(libhoop.Proxy)
 	if ok {
 		// Write to tty stdin content
 		if _, err := cmd.Write(pkt.Payload); err != nil {
-			log.Infof("session=%v | tty=true - failed copying stdin to tty, err=%v", string(sessionID), err)
-			a.sendCloseTerm(sessionID, "", "0")
+			log.With("sid", sid).Infof("tty=true - failed copying stdin to tty, err=%v", err)
+			a.sendCloseTerm(sid, "", "0")
 		}
 		return
 	}
 
-	spec := map[string][]byte{pb.SpecGatewaySessionID: []byte(sessionID)}
+	spec := map[string][]byte{pb.SpecGatewaySessionID: []byte(sid)}
 	stdoutWriter := pb.NewStreamWriter(a.client, pbclient.WriteStdout, spec)
 	opts := map[string]string{
 		"dlp_gcp_credentials": a.getGCPCredentials(),
@@ -47,49 +47,46 @@ func (a *Agent) doTerminalWriteAgentStdin(pkt *pb.Packet) {
 		// In case of sensitive contents in the error this could be propagate back
 		// to the client.
 		errMsg := fmt.Sprintf("failed executing command, reason=%v", err)
-		log.Infof("session=%s, tty=true - %s", sessionID, errMsg)
-		a.sendCloseTerm(sessionID, errMsg, "1")
+		log.With("sid", sid).Infof("tty=true - %s", errMsg)
+		a.sendCloseTerm(sid, errMsg, internalExitCode)
 		return
 	}
-	log.Infof("session=%s, tty=true - executing command:%v", sessionID, args)
+	log.With("sid", sid).Infof("tty=true - executing command:%v", args)
 	cmd.Run(func(exitCode int, msg string) {
 		if msg != "" {
-			log.Infof("session=%s, tty=true, exitcode=%v - err=%v", string(sessionID), exitCode, msg)
+			log.With("sid", sid).Infof("tty=true, exitcode=%v - err=%v", exitCode, msg)
 		}
-		a.sendCloseTerm(sessionID, msg, strconv.Itoa(exitCode))
+		a.sendCloseTerm(sid, msg, strconv.Itoa(exitCode))
 	})
 	a.connStore.Set(sessionIDKey, cmd)
 }
 
 func (a *Agent) doTerminalResizeTTY(pkt *pb.Packet) {
-	sessionID := string(pkt.Spec[pb.SpecGatewaySessionID])
-	sessionIDKey := fmt.Sprintf(cmdStoreKey, sessionID)
+	sid := string(pkt.Spec[pb.SpecGatewaySessionID])
+	sessionIDKey := fmt.Sprintf(cmdStoreKey, sid)
 	cmdObj := a.connStore.Get(sessionIDKey)
 	cmd, ok := cmdObj.(libhoop.Terminal)
 	if ok {
 		winSize, err := parsePttyWinSize(pkt.Payload)
 		if err != nil {
 			sentry.CaptureException(err)
-			log.Infof("session=%s, tty=true, winsize=%v - %v", sessionID, string(pkt.Payload), err)
+			log.With("sid", sid).Infof("tty=true, winsize=%v - %v", string(pkt.Payload), err)
 			return
 		}
 		if err := cmd.ResizeTTY(winSize); err != nil {
 			sentry.CaptureException(err)
-			log.Infof("session=%s, tty=true - failed resizing tty, err=%v", sessionID, err)
+			log.With("sid", sid).Infof("tty=true - failed resizing tty, err=%v", err)
 		}
 	}
 }
 
-func (a *Agent) sendCloseTerm(sessionID, msg, exitCode string) {
+func (a *Agent) sendCloseTerm(sid, msg, exitCode string) {
 	if msg != "" && exitCode == "" {
 		// must have an exit code if it has a msg
 		exitCode = "1"
 	}
-	var exitCodeKeyVal string
-	if exitCode != "" {
-		exitCodeKeyVal = fmt.Sprintf("%s=%s", pb.SpecClientExitCodeKey, exitCode)
-	}
-	a.sendClientSessionClose(sessionID, msg, exitCodeKeyVal)
+	log.With("sid", sid).Infof("exitcode=%v - err=%v", exitCode, msg)
+	a.sendClientSessionCloseWithExitCode(sid, msg, exitCode)
 }
 
 func parsePttyWinSize(winSizeBytes []byte) (*pty.Winsize, error) {
