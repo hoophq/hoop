08a03ff893e261c5c355a132a3864d3f0fd4e085
Rogerio Moura <roger0.rm@gmail.com>
2025-03-11 18:09:53 -0300

    chore(webapp): Add .cursor to .gitignore

--- webapp/src/gen/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export function Button({
-  children,
-  ...props
-}) {
-  return /*#__PURE__*/React.createElement("button", props, children);
-}
--- webapp/src/gen/config.js
+++
@@ -1,6 +0,0 @@
-// Re-export the ClojureScript config values
-
-export const config = {
-  webappUrl: window.config?.webappUrl || ''
-  // Add other configuration values as needed
-};
