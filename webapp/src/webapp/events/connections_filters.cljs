5db2148871452d107a9b6d4e2bb26ef8b17a15a0
Rogerio Moura <roger0.rm@gmail.com>
2025-03-26 10:07:26 -0300

    Add filtering and clear filters functionality to connection list

--- webapp/src/webapp/events/connections_filters.cljs
+++ webapp/src/webapp/events/connections_filters.cljs
@@ -18,7 +18,24 @@
                              (map (fn [[k v]]
                                     (str (name k) "=" v))
                                   query-params))))]
-     {:db (assoc-in db [:connections :loading] true)
+     {:db (-> db
+              (assoc-in [:connections :loading] true)
+              (assoc-in [:connections :filter-query] filters))
       :fx [[:dispatch [:fetch {:method "GET"
                                :uri uri
-                               :on-success #(rf/dispatch [:connections->set-connections %])}]]]})))
+                               :on-success #(rf/dispatch [:connections->set-filtered-connections %])}]]]})))
+
+(rf/reg-event-fx
+ :connections->set-filtered-connections
+ (fn
+   [{:keys [db]} [_ connections]]
+   {:db (-> db
+            (assoc-in [:connections :results] connections)
+            (assoc-in [:connections :loading] false))
+    :fx [[:dispatch [:connections->debug-log (str "Filtered connections: " (count connections))]]]}))
+
+(rf/reg-event-fx
+ :connections->debug-log
+ (fn [_ [_ message]]
+   (js/console.log message)
+   {}))
