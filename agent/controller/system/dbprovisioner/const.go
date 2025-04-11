ccf6a2ed438fa5c33863fe0518b0881ea4c4abab
San <sandromll@gmail.com>
2025-04-11 11:57:46 -0300

    AWS Connect: increase the timeout when connecting to databases to provision db roles

--- agent/controller/system/dbprovisioner/const.go
+++ agent/controller/system/dbprovisioner/const.go
@@ -1,5 +1,7 @@
 package dbprovisioner
 
+import "time"
+
 type roleNameType string
 
 const (
@@ -8,6 +10,8 @@ const (
 	readOnlyRoleName  roleNameType = "ro"
 	readWriteRoleName roleNameType = "rw"
 	adminRoleName     roleNameType = "ddl"
+
+	connectionTimeoutDuration = time.Second * 15
 )
 
 var roleNames = []roleNameType{readOnlyRoleName, readWriteRoleName, adminRoleName}
--- agent/controller/system/dbprovisioner/mysql.go
+++ agent/controller/system/dbprovisioner/mysql.go
@@ -48,13 +48,14 @@ func provisionMySQLRoles(r pbsystem.DBProvisionerRequest) *pbsystem.DBProvisione
 	}
 	defer db.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), connectionTimeoutDuration)
 	defer cancel()
 
 	// Ping actually tests the connection
 	err = db.PingContext(ctx)
 	if err != nil {
-		return pbsystem.NewError(r.SID, "failed to connect to engine %v: %v", r.DatabaseType, err)
+		return pbsystem.NewError(r.SID, "failed to connect to engine %v, host=%v, user=%v, reason=%v",
+			r.DatabaseType, r.DatabaseHostname, r.MasterUsername, err)
 	}
 
 	log.With("sid", r.SID, "engine", r.DatabaseType).Infof("starting provisioning roles")
@@ -78,7 +79,7 @@ func provisionMySQLRole(db *sql.DB, r pbsystem.DBProvisionerRequest, roleName ro
 		return pbsystem.NewResultError("failed generating SQL statement for user role %v: %v", userRole, err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), connectionTimeoutDuration)
 	defer cancel()
 	if _, err := db.ExecContext(ctx, statement); err != nil {
 		return pbsystem.NewResultError(err.Error())
