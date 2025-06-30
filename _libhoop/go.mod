9409330e0c6150562936f7c7767372dd8d89e5d4
Rogerio Moura <roger0.rm@gmail.com>
2025-06-30 14:05:47 -0300

    Remove zap logger dependency and associated logging functions

--- _libhoop/go.mod
+++ _libhoop/go.mod
@@ -4,7 +4,4 @@ go 1.22.4
 
 require (
 	github.com/creack/pty v1.1.21
-	go.uber.org/zap v1.27.0
 )
-
-require go.uber.org/multierr v1.10.0 // indirect
--- _libhoop/llog/llog.go
+++ _libhoop/llog/llog.go
@@ -1,36 +1,4 @@
 package llog
 
-import (
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
-)
-
-var (
-	mockLogger = zap.NewNop()
-	sugar      = mockLogger.Sugar()
-
-	Printf  = sugar.Infof
-	Println = sugar.Info
-	Debug   = sugar.Debug
-	Debugf  = sugar.Debugf
-	Infof   = sugar.Infof
-	Info    = sugar.Info
-	Warnf   = sugar.Warnf
-	Warn    = sugar.Warn
-	Error   = sugar.Error
-	Errorf  = sugar.Errorf
-	Fatalf  = sugar.Fatalf
-	Fatal   = sugar.Fatal
-	With    = sugar.With
-)
-
-func NewHumanEncoder(cfg zapcore.EncoderConfig) zapcore.Encoder {
-	return zapcore.NewConsoleEncoder(cfg)
-}
-
-func NewVerboseEncoder(cfg zapcore.EncoderConfig) zapcore.Encoder {
-	return zapcore.NewConsoleEncoder(cfg)
-}
-
 func ReinitializeLogger() {
 }
