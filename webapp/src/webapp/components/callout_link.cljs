9e489607e9cd7dd3cbf6ca65e0bd3f1ddfee3e9b
Rogerio Moura <roger0.rm@gmail.com>
2025-07-14 17:13:18 -0300

    feat: Add CalloutLink component and integrate it into the authentication general tab

---
+++ webapp/src/webapp/components/callout_link.cljs
@@ -0,0 +1,13 @@
+(ns webapp.components.callout-link
+  (:require
+   ["@radix-ui/themes" :refer [Callout Link]]
+   ["lucide-react" :refer [ArrowUpRight]]))
+
+(defn main [{:keys [href on-click text]}]
+  [:> Link {:href href
+            :target "_blank"}
+   [:> Callout.Root {:size "1" :mt "4" :variant "outline" :color "gray" :class "w-fit"}
+    [:> Callout.Icon
+     [:> ArrowUpRight {:size 16}]]
+    [:> Callout.Text
+     text]]])
--- webapp/src/webapp/features/authentication/views/providers/form_fields.cljs
+++ webapp/src/webapp/features/authentication/views/providers/form_fields.cljs
@@ -1,6 +1,6 @@
 (ns webapp.features.authentication.views.providers.form-fields
   (:require
-   ["@radix-ui/themes" :refer [Badge Box Button Flex Text]]
+   ["@radix-ui/themes" :refer [Badge Box Button Flex Heading Grid Switch Text]]
    ["lucide-react" :refer [X]]
    [re-frame.core :as rf]
    [reagent.core :as r]
@@ -61,45 +61,106 @@
 
 (defn provider-form [provider]
   (let [config (rf/subscribe [:authentication->provider-config])
+        advanced-config (rf/subscribe [:authentication->advanced-config])
         provider-fields (get-in provider-configs [provider :fields] [])]
     [:> Box
      ;; Common fields for all providers
-     [:> Box {:class "space-y-radix-4"}
+     [:> Box {:class "space-y-radix-9"}
       ;; Client ID
-      [:> Box
-       [forms/input
-        {:label "Client ID"
-         :placeholder "OAuth application identifier from your identity provider."
-         :value (:client-id @config)
-         :on-change #(rf/dispatch [:authentication->update-config-field
-                                   :client-id (-> % .-target .-value)])}]]
+      [:> Grid {:columns "7" :gap "7"}
+       [:> Box {:grid-column "span 2 / span 2"}
+        [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
+         "Client ID"]
+        [:> Text {:size "3" :class "text-[--gray-11]"}
+         "OAuth application identifier from your identity provider."]]
+
+       [:> Box {:grid-column "span 5 / span 5"}
+        [forms/input
+         {:full-width? true
+          :value (:client-id @config)
+          :on-change #(rf/dispatch [:authentication->update-config-field
+                                    :client-id (-> % .-target .-value)])}]]]
 
       ;; Client Secret
-      [:> Box
-       [forms/input
-        {:label "Client Secret"
-         :placeholder "Secret key from your OAuth identity provider."
-         :type "password"
-         :value (:client-secret @config)
-         :on-change #(rf/dispatch [:authentication->update-config-field
-                                   :client-secret (-> % .-target .-value)])}]]
+      [:> Grid {:columns "7" :gap "7"}
+       [:> Box {:grid-column "span 2 / span 2"}
+        [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
+         "Client Secret"]
+        [:> Text {:size "3" :class "text-[--gray-11]"}
+         "Secret key from your OAuth identity provider."]]
 
-      ;; Provider-specific fields
-      (for [field provider-fields]
-        ^{:key (:name field)}
-        [:> Box
-         [forms/input
-          {:label (:label field)
-           :placeholder (:placeholder field)
-           :value (get @config (:name field))
-           :on-change #(rf/dispatch [:authentication->update-config-field
-                                     (:name field) (-> % .-target .-value)])}]
-         (when (:description field)
-           [:> Text {:size "2" :class "text-[--gray-11] mt-1"}
-            (:description field)])])
+       [:> Box {:grid-column "span 5 / span 5"}
+        [forms/input
+         {:full-width? true
+          :type "password"
+          :value (:client-secret @config)
+          :on-change #(rf/dispatch [:authentication->update-config-field
+                                    :client-secret (-> % .-target .-value)])}]]]
+
+
+      [:> Grid {:columns "7" :gap "7"}
+       [:> Box {:grid-column "span 2 / span 2"}
+        [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
+         "Custom Scopes"]
+        [:> Text {:size "3" :class "text-[--gray-11]"}
+         "Additional OAuth scopes to request during login."]]
+
+       [:> Box {:grid-column "span 5 / span 5"}
+        [forms/input
+         {:full-width? true
+          :type "password"
+          :value (:client-secret @config)
+          :on-change #(rf/dispatch [:authentication->update-config-field
+                                    :client-secret (-> % .-target .-value)])}]]]
+
+      [:> Grid {:columns "7" :gap "7"}
+       [:> Box {:grid-column "span 2 / span 2"}
+        [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
+         "Audience (Optional)"]
+        [:> Text {:size "3" :class "text-[--gray-11]"}
+         "API identifier for token validation (required by some providers like Auth0)."]]
+
+       [:> Box {:grid-column "span 5 / span 5"}
+        [forms/input
+         {:full-width? true
+          :type "password"
+          :value (:client-secret @config)
+          :on-change #(rf/dispatch [:authentication->update-config-field
+                                    :client-secret (-> % .-target .-value)])}]]]
+
+      [:> Grid {:columns "7" :gap "7"}
+       [:> Box {:grid-column "span 2 / span 2"}
+        [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
+         "Hoop Local Authentication"]
+        [:> Text {:size "3" :class "text-[--gray-11]"}
+         "Turn off Hoop native authentication to manage it only with Identity Providers."]]
+
+       [:> Box {:class "space-y-radix-4" :grid-column "span 5 / span 5"}
+        [:> Flex {:align "center" :gap "3"}
+         [:> Switch {:checked (not= (:local-auth-enabled @advanced-config) false)
+                     :onCheckedChange #(rf/dispatch [:authentication->toggle-local-auth %])}]
+         [:> Text {:size "3" :weight "medium"}
+          (if (not= (:local-auth-enabled @advanced-config) false) "On" "Off")]]
+
+        [:> Text {:size "2" :class "text-[--gray-11]"}
+         "When turned on, users list might be outdated until users sign up."]]]
+
+      ;; ;; Provider-specific fields
+      ;; (for [field provider-fields]
+      ;;   ^{:key (:name field)}
+      ;;   [:> Box
+      ;;    [forms/input
+      ;;     {:label (:label field)
+      ;;      :placeholder (:placeholder field)
+      ;;      :value (get @config (:name field))
+      ;;      :on-change #(rf/dispatch [:authentication->update-config-field
+      ;;                                (:name field) (-> % .-target .-value)])}]
+      ;;    (when (:description field)
+      ;;      [:> Text {:size "2" :class "text-[--gray-11] mt-1"}
+      ;;       (:description field)])])
 
       ;; Custom Scopes
-      [:> Box
-       [custom-scopes-input
-        {:scopes (or (:custom-scopes @config) ["email" "profile"])
-         :on-change #(rf/dispatch [:authentication->update-config-field :custom-scopes %])}]]]]))
+      #_[:> Box
+         [custom-scopes-input
+          {:scopes (or (:custom-scopes @config) ["email" "profile"])
+           :on-change #(rf/dispatch [:authentication->update-config-field :custom-scopes %])}]]]]))
