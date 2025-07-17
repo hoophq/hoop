27c9c54e38db2e8b2e18a39cfb694aba674ef2a9
Rogerio Moura <roger0.rm@gmail.com>
2025-07-17 14:40:34 -0300

    feat: normalize path before adding to connection in runbooks feature

--- webapp/src/webapp/features/runbooks/events.cljs
+++ webapp/src/webapp/features/runbooks/events.cljs
@@ -2,26 +2,34 @@
   (:require
    [re-frame.core :as rf]))
 
+(defn normalize-path
+  "Remove leading slash from path if present"
+  [path]
+  (if (and (string? path) (not (empty? path)) (= (first path) "/"))
+    (subs path 1)
+    path))
+
 (rf/reg-event-fx
  :runbooks/add-path-to-connection
  (fn [{:keys [db]} [_ {:keys [path connection-id]}]]
-   (let [plugin (get-in db [:plugins->plugin-details :plugin])
+   (let [normalized-path (normalize-path path)
+         plugin (get-in db [:plugins->plugin-details :plugin])
          connections (or (:connections plugin) [])
          connection-exists? (some #(= (:id %) connection-id) connections)
          updated-connections (if connection-exists?
                                ;; Update existing connection
                                (map (fn [conn]
                                       (if (= (:id conn) connection-id)
-                                        (if (or (nil? path) (empty? path))
+                                        (if (or (nil? normalized-path) (empty? normalized-path))
                                           (assoc conn :config nil)
-                                          (update conn :config (fn [_] [path])))
+                                          (update conn :config (fn [_] [normalized-path])))
                                         conn))
                                     connections)
                                ;; Add new connection to existing list
                                (conj connections {:id connection-id
-                                                  :config (if (or (nil? path) (empty? path))
+                                                  :config (if (or (nil? normalized-path) (empty? normalized-path))
                                                             nil
-                                                            [path])}))
+                                                            [normalized-path])}))
          updated-plugin (assoc plugin :connections (vec updated-connections))]
      {:fx [[:dispatch [:plugins->update-plugin updated-plugin]]]})))
 
