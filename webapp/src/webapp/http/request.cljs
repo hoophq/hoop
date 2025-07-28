b64d24c71bdea3a38818d570008f8436721a4c7c
Rogerio Moura <roger0.rm@gmail.com>
2025-07-28 14:29:22 -0300

    fix: Improve error handling in database schema events and response parsing
    - Updated error dispatching in database schema events to remove unnecessary wrapping in a map.
    - Refactored response handling in HTTP requests to provide complete error objects to on-failure callbacks, enhancing error management across the application.

--- webapp/src/webapp/http/request.cljs
+++ webapp/src/webapp/http/request.cljs
@@ -12,46 +12,31 @@
   "This functions has two possible outcomes:
   1 - When the status is 401 (Unauthorized), it redirects user to the logout page
   2 - when the status is 399 or below, it executes a on-failure function, that is provided by upperscope."
-  [{:keys  [status on-failure]}]
+  [{:keys [status on-failure]}]
   (when (= status 401)
     (rf/dispatch [:navigate :logout-hoop]))
   (when (> status 399) (on-failure)))
 
-(defmulti response-parser identity)
-(defmethod response-parser "application/json" [_ response on-success]
+(defn parse-json-response
+  [response on-success on-failure]
   (.then
    (.json response)
    (fn [json]
      (let [payload (js->clj json :keywordize-keys true)]
-       (when (not (.-ok response))
+       (if (.-ok response)
+         (on-success payload (.-headers response))
          (not-ok {:status (.-status response)
-                  :on-failure #(throw (js/Error. (js/JSON.stringify json)))}))
-       (on-success payload (.-headers response))
-       payload))
-
-   (fn [json]
-     (let [payload (js->clj json :keywordize-keys true)]
-       (if (not (.-ok response))
-         (not-ok {:status (.-status response)
-                  :on-failure #(throw (js/Error. payload))})
-         (on-success payload (.-headers response)))))))
-
-;;TODO send headers object as second param of on-success and on-failure
-(defmethod response-parser :default [_ response on-success]
-  (.then
-   (.text response)
-   (fn [text]
-     (when (not (.-ok response)) (not-ok {:status (.-status response)
-                                          :on-failure #(throw (js/Error. text))}))
-     (on-success text (.-headers response))
-     text)
-
-   (fn [json]
-     (let [payload (js->clj json :keywordize-keys true)]
-       (if (not (.-ok response))
-         (not-ok {:status (.-status response)
-                  :on-failure #(throw (js/Error. payload))})
-         (on-success payload (.-headers response)))))))
+                  :on-failure #(on-failure payload)}))))
+   (fn [error]
+     ;; JSON parsing failed
+     (.then
+      (.text response)
+      (fn [text]
+        (let [error-payload {:message text :status (.-status response)}]
+          (if (.-ok response)
+            (on-success text (.-headers response))
+            (not-ok {:status (.-status response)
+                     :on-failure #(on-failure error-payload)}))))))))
 
 (defn query-params-parser
   [queries]
@@ -60,19 +45,16 @@
       (str "?" (.toString url-search-params))
       "")))
 
-(defmulti response-by-method (fn [method _response _on-success _on-failure] method))
-
-(defmethod response-by-method "HEAD" [_ response on-success _]
-  ;; HEAD requests only need headers, no body parsing
-  (when (.-ok response)
-    (on-success response (.-headers response)))
-  response)
-
-(defmethod response-by-method :default [_ response on-success on-failure]
-  (let [content-type (.. response -headers (get "content-type"))]
-    (if (and content-type (re-find #"application/json" content-type))
-      (response-parser "application/json" response on-success on-failure)
-      (response-parser :default response on-success on-failure))))
+(defn handle-response
+  [method response on-success on-failure]
+  (if (= method "HEAD")
+    ;; HEAD requests only need headers, no body parsing
+    (do
+      (when (.-ok response)
+        (on-success response (.-headers response)))
+      response)
+    ;; All other methods expect JSON
+    (parse-json-response response on-success on-failure)))
 
 (defn request
   "request abstraction for making a http request
@@ -82,12 +64,13 @@
   :url -> URL to be called
   :body -> a clojure map of the body structure
   :on-sucess -> callback that receives as argument the response payload
-  :on-failure -> callback that has one argument that is the error message to treat 4xx and 5xx status codes. If not provided, a default callback will be called
+  :on-failure -> callback that receives the complete error object (not just the :message)
   :options -> this is a map of options, like headers
 
   it returns a promise with the response in a clojure map and executes a on-sucess callback"
   [{:keys [method url body query-params on-success on-failure options]}]
-  (let [json-body (.stringify js/JSON (clj->js body))]
+  (let [json-body (.stringify js/JSON (clj->js body))
+        actual-on-failure (or on-failure error-handling)]
     (.catch
      (.then
       (js/fetch (str url (query-params-parser query-params))
@@ -97,16 +80,13 @@
                                                   (not= method "HEAD"))]
                                   {:body json-body}))))
       (fn [response]
-        (response-by-method method response on-success on-failure)))
+        (handle-response method response on-success actual-on-failure)))
      (fn [error]
-       (if (= (.-message error) "Failed to fetch")
-         (if (= on-failure nil)
-           (error-handling (js/JSON.parse (.-message error)))
-           (on-failure (:message (.-message error))))
-
-         (let [error-res (js/JSON.parse (.-message error))
-               error-edn (js->clj error-res :keywordize-keys true)]
-           (if (= on-failure nil)
-             (error-handling error-edn)
-             (on-failure (:message error-edn) error-edn))))))))
+       (let [error-payload (if (= (.-message error) "Failed to fetch")
+                             {:message "Network error: Failed to fetch" :type "network-error"}
+                             (try
+                               (js->clj (js/JSON.parse (.-message error)) :keywordize-keys true)
+                               (catch js/Error _
+                                 {:message (.-message error) :type "unknown-error"})))]
+         (actual-on-failure error-payload))))))
 
