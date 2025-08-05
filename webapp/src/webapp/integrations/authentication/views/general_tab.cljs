2dd71481c770e48e5710fcc821800a596460395c
Rogerio Moura <roger0.rm@gmail.com>
2025-08-05 14:52:13 -0300

    feat: Add SVG icons for identity providers (Auth0, AWS Cognito, Azure, Google, JumpCloud, Okta) and update authentication views to utilize these icons in the provider selection grid

--- webapp/src/webapp/integrations/authentication/views/general_tab.cljs
+++ webapp/src/webapp/integrations/authentication/views/general_tab.cljs
@@ -1,7 +1,7 @@
 (ns webapp.integrations.authentication.views.general-tab
   (:require
    ["@radix-ui/themes" :refer [Box Grid Heading Text]]
-   ["lucide-react" :refer [Key Users]]
+   ["lucide-react" :refer [BookLock GlobeLock]]
    [re-frame.core :as rf]
    [reagent.core :as r]
    [webapp.components.callout-link :as callout-link]
@@ -9,24 +9,35 @@
    [webapp.config :as config]
    [webapp.integrations.authentication.views.providers.form-fields :as provider-forms]))
 
-#_(defn identity-provider-grid []
-    (let [selected-provider (rf/subscribe [:authentication->selected-provider])]
-      [:> Box
-       [:> Grid {:columns "3" :gap "3" :mb "6"}
-        ;; TODO: Replace with actual provider icons
-        (doall
-         (for [provider [{:id "auth0" :name "Auth0"}
-                         {:id "aws-cognito" :name "AWS Cognito"}
-                         {:id "azure" :name "Azure"}
-                         {:id "google" :name "Google"}
-                         {:id "jumpcloud" :name "JumpCloud"}
-                         {:id "okta" :name "Okta"}]]
-           ^{:key (:id provider)}
-           [selection-card/selection-card
-            {:icon (r/as-element [:> Users {:size 18}])
-             :title (:name provider)
-             :selected? (= @selected-provider (:id provider))
-             :on-click #(rf/dispatch [:authentication->set-provider (:id provider)])}]))]]))
+(defn icon-component [icon-path selected?]
+  (r/as-element [:figure {:class "w-5"}
+                 [:img {:class (when selected? "invert")
+                        :src (str config/webapp-url icon-path)}]]))
+
+(defn identity-provider-grid []
+  (let [selected-provider (rf/subscribe [:authentication->selected-provider])]
+    [:> Box
+     [:> Grid {:columns "3" :gap "3" :mb "6"}
+      (doall
+       (for [provider [{:id "auth0" :name "Auth0" :icon [icon-component "/icons/identity-providers/auth0.svg"
+                                                         (= @selected-provider "auth0")]}
+                       {:id "aws-cognito" :name "AWS Cognito" :icon [icon-component "/icons/identity-providers/aws-cognito.svg"
+                                                                     (= @selected-provider "aws-cognito")]}
+                       {:id "azure" :name "Azure" :icon [icon-component "/icons/identity-providers/azure.svg"
+                                                         (= @selected-provider "azure")]}
+                       {:id "google" :name "Google" :icon [icon-component "/icons/identity-providers/google.svg"
+                                                           (= @selected-provider "google")]}
+                       {:id "jumpcloud" :name "JumpCloud" :icon [icon-component "/icons/identity-providers/jumpcloud.svg"
+                                                                 (= @selected-provider "jumpcloud")]}
+                       {:id "okta" :name "Okta" :icon [icon-component "/icons/identity-providers/okta.svg"
+                                                       (= @selected-provider "okta")]}
+                       {:id "other" :name "Other" :icon [:> GlobeLock {:size 20}]}]]
+         ^{:key (:id provider)}
+         [selection-card/selection-card
+          {:icon (r/as-element (:icon provider))
+           :title (:name provider)
+           :selected? (= @selected-provider (:id provider))
+           :on-click #(rf/dispatch [:authentication->set-provider (:id provider)])}]))]]))
 
 (defn main []
   (let [auth-method (rf/subscribe [:authentication->auth-method])
@@ -46,7 +57,7 @@
       [:> Box {:class "space-y-radix-4" :grid-column "span 5 / span 5"}
        ;; Local authentication card
        [selection-card/selection-card
-        {:icon (r/as-element [:> Key {:size 18}])
+        {:icon (r/as-element [:> BookLock {:size 18}])
          :title "Local authentication"
          :description "Create and manage accounts directly in Hoop"
          :selected? (= @auth-method "local")
@@ -54,7 +65,7 @@
 
        ;; Identity Provider card
        [selection-card/selection-card
-        {:icon (r/as-element [:> Users {:size 18}])
+        {:icon (r/as-element [:> GlobeLock {:size 18}])
          :title "Identity Provider"
          :description "Integrate with your existing SSO solution (Auth0, Okta, Google and more)"
          :selected? (= @auth-method "identity-provider")
@@ -63,15 +74,15 @@
      ;; Identity Provider Configuration (shown when selected)
      (when (= @auth-method "identity-provider")
        [:<>
-        #_[:> Grid {:columns "7" :gap "7"}
-           [:> Box {:grid-column "span 2 / span 2"}
-            [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
-             "Identity Provider"]
-            [:> Text {:size "3" :class "text-[--gray-11]"}
-             "Choose the identity provider that manages your organization's user accounts."]]
+        [:> Grid {:columns "7" :gap "7"}
+         [:> Box {:grid-column "span 2 / span 2"}
+          [:> Heading {:as "h3" :size "4" :weight "bold" :class "text-[--gray-12]"}
+           "Identity Provider"]
+          [:> Text {:size "3" :class "text-[--gray-11]"}
+           "Choose the identity provider that manages your organization's user accounts."]]
 
-           [:> Box {:class "space-y-radix-7" :grid-column "span 5 / span 5"}
-            [identity-provider-grid]]]
+         [:> Box {:class "space-y-radix-7" :grid-column "span 5 / span 5"}
+          [identity-provider-grid]]]
 
         (when @selected-provider
           [provider-forms/provider-form @selected-provider])])]))
