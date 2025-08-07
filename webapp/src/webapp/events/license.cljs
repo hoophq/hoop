abcb596d72daa0f63436c1351ca30cd0add28ca4
Rogerio Moura <roger0.rm@gmail.com>
2025-08-07 15:24:29 -0300

    feat: Enhance license update event with error handling
    - Added error handling to the `:license->update-license-key` event to manage invalid JSON format gracefully.
    - Implemented a snackbar notification to inform users of errors during license processing.
    - Maintained existing functionality while improving user feedback and robustness of the event handling.

--- webapp/src/webapp/events/license.cljs
+++ webapp/src/webapp/events/license.cljs
@@ -2,11 +2,17 @@
   (:require [re-frame.core :as rf]))
 
 (rf/reg-event-fx
-  :license->update-license-key
-  (fn [_ [_ license-info]]
-    (let [license-obj (.parse js/JSON license-info)]
-    {:fx [[:dispatch
-           [:fetch {:method "PUT"
-                    :uri "/orgs/license"
-                    :body license-obj
-                    :on-success #(rf/dispatch [:gateway->get-info %])}]]]})))
+ :license->update-license-key
+ (fn [_ [_ license-info]]
+   (try
+     (let [license-obj (.parse js/JSON license-info)]
+       {:fx [[:dispatch
+              [:fetch {:method "PUT"
+                       :uri "/orgs/license"
+                       :body license-obj
+                       :on-success #(rf/dispatch [:gateway->get-info %])}]]]})
+     (catch js/Error e
+       {:fx [[:dispatch
+              [:show-snackbar {:level :error
+                               :text "Error processing license: invalid JSON format"
+                               :details {:error (.-message e)}}]]]}))))
