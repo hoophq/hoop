b896f4ccb22656d838ad4dee05be6692fd13cbda
Emanuel Júnior <emanuelmpjr@gmail.com>
2025-09-18 16:35:08 -0300

    feat: add default value for http_server_config in serverconfig table

--- rootfs/app/migrations/000045_http_connection_credentials.up.sql
+++ rootfs/app/migrations/000045_http_connection_credentials.up.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-ALTER TABLE private.serverconfig ADD COLUMN http_server_config JSONB NULL;
+ALTER TABLE private.serverconfig ADD COLUMN http_server_config JSONB NULL DEFAULT '{"listen_address": "localhost:8080"}';
 
 COMMIT;
