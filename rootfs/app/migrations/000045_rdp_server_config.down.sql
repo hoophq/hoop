c7c3d50e28ac2d71bbdf90e55f5491f7dfd02103
matheusfrancisco <matheusmachadoufsc@gmail.com>
2025-10-02 09:33:44 -0300

    add: default value on rdp_server_config

--- rootfs/app/migrations/000045_rdp_server_config.down.sql
+++ rootfs/app/migrations/000045_rdp_server_config.down.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
-ALTER TABLE private.serverconfig DROP COLUMN rdp_server_config;
+ALTER TABLE private.serverconfig ADD COLUMN rdp_server_config JSONB DEFAULT '{"listen_address":"0.0.0.0:3389"}';
+
 
 COMMIT;
