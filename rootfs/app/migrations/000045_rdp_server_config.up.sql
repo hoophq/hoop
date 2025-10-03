9ec86dfd82c3cd78abb23d8a87ce759c37ab1b5e
Matheus Francisco <matheusmachadoufsc@gmail.com>
2025-10-03 17:22:48 -0300

    Update rootfs/app/migrations/000045_rdp_server_config.up.sql

--- rootfs/app/migrations/000045_rdp_server_config.up.sql
+++ rootfs/app/migrations/000045_rdp_server_config.up.sql
@@ -1,4 +1,4 @@
 BEGIN;
 
-ALTER TABLE private.serverconfig ADD COLUMN rdp_server_config JSONB DEFAULT '{"listen_address":"0.0.0.0:13389"}'
+ALTER TABLE private.serverconfig ADD COLUMN rdp_server_config JSONB DEFAULT '{"listen_address":"0.0.0.0:13389"}';
 COMMIT;
