84068e11d064f1d852aa19fdbd6a3edef0b2f6f2
Emanuel Júnior <emanuelmpjr@gmail.com>
2025-10-07 14:48:48 -0300

    refactor: add type in the migration clause

--- rootfs/app/migrations/000046_remove_oracle_ld.up.sql
+++ rootfs/app/migrations/000046_remove_oracle_ld.up.sql
@@ -5,7 +5,7 @@ SET envs = ev.envs::jsonb - 'envvar:LD_LIBRARY_PATH'
 WHERE ev.id IN (
   SELECT c.id
   FROM private.connections c
-  WHERE c.subtype = 'oracledb'
+  WHERE c.type = 'database' AND c.subtype = 'oracledb'
 );
 
 COMMIT;
