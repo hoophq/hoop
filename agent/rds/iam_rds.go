7944e885cb668267bbd5d4d8efc79246a0f93471
matheusfrancisco <matheusmachadoufsc@gmail.com>
2025-11-17 19:03:12 -0300

    add: get region from the host rds

--- agent/rds/iam_rds.go
+++ agent/rds/iam_rds.go
@@ -13,24 +13,19 @@ import (
 func BuildRdsEnvAuth(env map[string]any) (map[string]any, error) {
 	userEnv, ok := env["envvar:USER"].(string)
 	if !ok {
-		return nil, fmt.Errorf("aws rds-user not found in env")
+		return nil, fmt.Errorf("_aws_iam_rds: not found in envvar")
 	}
 
 	decodedEnv, err := base64.StdEncoding.DecodeString(fmt.Sprintf("%v", userEnv))
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode rds-user env: %v", err)
+		return nil, fmt.Errorf("failed to decode rds iam user env: %v", err)
 	}
 
 	values := strings.Split(string(decodedEnv), ":")
 	if len(values) != 2 {
-		return nil, fmt.Errorf("invalid rds-user env format")
+		return nil, fmt.Errorf("invalid rds iam user format env format")
 	}
 	user := values[1]
-
-	region, ok := "us-west-2", true
-	if !ok {
-		return nil, fmt.Errorf("aws region not found in env")
-	}
 
 	encodedHost, ok := env["envvar:HOST"].(string)
 	host, err := base64.StdEncoding.DecodeString(fmt.Sprintf("%v", encodedHost))
@@ -39,6 +34,10 @@ func BuildRdsEnvAuth(env map[string]any) (map[string]any, error) {
 		return nil, fmt.Errorf("aws rds host not found in env")
 	}
 
+	region, err := regionFromHost(string(host))
+	if err != nil {
+		return nil, fmt.Errorf("aws region not found in the host")
+	}
 	encodedPort, ok := env["envvar:PORT"].(string)
 	port, err := base64.StdEncoding.DecodeString(fmt.Sprintf("%v", encodedPort))
 	if !ok {
@@ -55,6 +54,15 @@ func BuildRdsEnvAuth(env map[string]any) (map[string]any, error) {
 
 	return env, nil
 
+}
+
+func regionFromHost(host string) (string, error) {
+	parts := strings.Split(host, ".")
+	// example: ["pgtestiam", "cqvjxizuxvwe", "us-west-2", "rds", "amazonaws", "com"]
+	if len(parts) < 3 {
+		return "", fmt.Errorf("unable to parse region from host: %s", host)
+	}
+	return parts[2], nil
 }
 
 func generateToken(host, region, port, user string) (string, error) {
