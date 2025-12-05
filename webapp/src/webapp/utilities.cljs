5ea5031dd5136790057b3758890b6dd213bac29a
Luan Lorenzo <luanlorenzo23@gmail.com>
2025-12-05 11:39:19 -0300

    fix: enhance base64 decoding to handle session blob output (#1180)

--- webapp/src/webapp/utilities.cljs
+++ webapp/src/webapp/utilities.cljs
@@ -22,42 +22,13 @@
          (map capitalize)
          (clojure.string/join " "))))
 
-(defn classes-of
-  "Get the classes of an element as a Clojure keyword vector."
-  [e]
-  (let [words (-> e (.getAttribute "class") (string/split " "))]
-    (mapv keyword words)))
-
-(defn classes->str
-  "Change a Clojure keyword seq into an HTML class string."
-  [classes]
-  (->> classes (mapv name) (string/join " ")))
-
-(defn class-reset!
-  "Unconditionally set the classes of an element."
-  [e classes]
-  (.setAttribute e "class" (classes->str classes))
-  e)
-
-(defn class-swap!
-  "Update the classes of an element using a fn."
-  [e f]
-  (class-reset! e (f (classes-of e))))
-
-(defn add-class!
-  "Add a class to an element."
-  [e class]
-  (class-swap! e #(distinct (conj % (keyword class)))))
-
-(defn remove-class!
-  "Remove a class from an element."
-  [e class]
-  (class-swap! e (fn [current] (remove #(= % (keyword class)) current))))
-
 (defn cn [& inputs]
   (twMerge (apply clsx inputs)))
 
 (defn decode-b64 [data]
   (try
-    (string/replace (js/decodeURIComponent (js/escape (js/atob data))) #"∞" "\t")
-    (catch js/Error _ (str ""))))
+    (let [decoded (js/atob data)]
+      (try
+        (string/replace (js/decodeURIComponent (js/escape decoded)) #"∞" "\t")
+        (catch js/Error _ decoded)))
+    (catch js/Error _ "")))
