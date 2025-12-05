24a1c54c1314c6892283f8747b8956ec4dc50965
Sandro Mello <sandromll@gmail.com>
2025-12-05 14:08:53 -0300

    fix: migration 52 returning error when database contains legacy user_gruops views (1.35.22 and above) (#1181)

--- rootfs/app/migrations/000052_user_groups_name_size.up.sql
+++ rootfs/app/migrations/000052_user_groups_name_size.up.sql
@@ -1,1 +1,8 @@
+-- very old legacy views cleanup - versions before 1.35.22
+-- the migration will fail if those view exists
+DROP VIEW IF EXISTS public.user_groups CASCADE;
+DROP VIEW IF EXISTS public_a.user_groups CASCADE;
+DROP VIEW IF EXISTS public_b.user_groups CASCADE;
+
+-- modify column size
 ALTER TABLE private.user_groups ALTER COLUMN name TYPE VARCHAR(180);
