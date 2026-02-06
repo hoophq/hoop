c7e097f4a63fc6443631746d7ccaabb1e61242cf
Rogerio Moura <roger0.rm@gmail.com>
2026-02-06 16:39:31 -0300

    feat: add environment variable management and enhance Claude Code form handling

--- webapp/src/webapp/connections/views/setup/events/process_form.cljs
+++ webapp/src/webapp/connections/views/setup/events/process_form.cljs
@@ -24,6 +24,7 @@
         (= subtype "git")
         (= subtype "github")) "application"
     :else (case ui-type
+            "httpproxy" "httpproxy"
             "network" "application"
             "server" "custom"
             "database" "database"
