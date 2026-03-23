044a3d2108e673fb62bef604f4128bda9e48afe5
Emanuel Júnior <emanuelmpjr@gmail.com>
2026-03-23 12:27:29 -0300

    fix: add missing tables

--- rootfs/app/migrations/000067_attributes.down.sql
+++ rootfs/app/migrations/000067_attributes.down.sql
@@ -3,6 +3,9 @@ BEGIN;
 SET search_path TO private;
 
 DROP TABLE IF EXISTS connections_attributes;
+DROP TABLE IF EXISTS access_request_rules_attributes;
+DROP TABLE IF EXISTS guardrail_rules_attributes;
+DROP TABLE IF EXISTS datamasking_rules_attributes;
 DROP TABLE IF EXISTS attributes;
 
 COMMIT;
