33d2a1d1c065d986bf725bac7101b19a72e57d84
Matheus Francisco <matheusmachadoufsc@gmail.com>
2026-04-20 11:16:23 -0300

    http proxy subdomain access (#1390)
    * add: showing the user about the wildcard
    
    * Update webapp/src/webapp/connections/native_client_access/main.cljs
    
    * Update webapp/src/webapp/connections/native_client_access/main.cljs
    
    * Update gateway/api/connections/connection_credentials.go
    
    * Update webapp/src/webapp/connections/native_client_access/main.cljs

--- webapp/src/webapp/connections/native_client_access/main.cljs
+++ webapp/src/webapp/connections/native_client_access/main.cljs
@@ -213,7 +213,7 @@
   "Http proxy specific credentials fields"
   [{:keys [command port proxy_token]}]
 
-  (let [{:keys [curl browser]} (some-> command js/JSON.parse (js->clj :keywordize-keys true))]
+  (let [{:keys [curl browser subdomain]} (some-> command js/JSON.parse (js->clj :keywordize-keys true))]
     [:> Box {:class "space-y-4"}
 
      ;; Host
@@ -248,6 +248,14 @@
        {:status :success
         :id "command-browser"
         :logs browser}]]
+
+     [:> Box {:class "space-y-2"}
+      [:> Text {:size "2" :weight "bold" :class "text-[--gray-12]"}
+       "Command Subdomain Browser"]
+      [logs/new-container
+       {:status :success
+        :id "command-wildcard-browser"
+        :logs subdomain}]]
 
      ;; Port
      [:> Box {:class "space-y-2"}
