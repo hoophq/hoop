2e627668dbfcbc929ba784c68f5afb558bb02cb1
Emanuel <emanuelmpjr@gmail.com>
2026-05-15 16:50:50 -0300

    refactor: re-implement rulepack ownership checks in data masking and guardrail APIs

--- rootfs/app/migrations/000089_rulepack.down.sql
+++ rootfs/app/migrations/000089_rulepack.down.sql
@@ -4,6 +4,7 @@ SET search_path TO private;
 
 DROP INDEX IF EXISTS idx_attributes_rulepack_id;
 ALTER TABLE attributes DROP COLUMN IF EXISTS rulepack_id;
+
 DROP TABLE IF EXISTS rulepacks;
 
 COMMIT;
